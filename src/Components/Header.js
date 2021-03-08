e0409b4dbe23bd1013447e75b122b4120e5029dc
stephp <stephanie.penev@gmail.com>
2021-03-07 22:00:40 -0500

    added Components files and worked on JS

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Compass</h1>
+      <h2>A Daily Intention-Setting Guide</h2>
+    </header>
+  );
+};
+
+export default Header;
