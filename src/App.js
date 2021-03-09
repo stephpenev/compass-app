1d5189731e6dbb38eb3d3e341a05cd1226b7a179
stephp <stephanie.penev@gmail.com>
2021-03-08 21:14:41 -0500

    added date functionality and connected to input submit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import './App.css';
 import firebase from './firebase.js';
+import React from "react";
 import Header from './Components/Header.js';
 // import IntentionForm from './Components/IntentionForm.js';
 // import IntentionInput from "./Components/IntentionInput.js";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import Footer from './Components/Footer';
 import { useEffect, useState } from 'react';
 
 function App() {
-  // OUTLINE OF FLOW
+  // SCOPE --
   // An app which holds daily intentions
   // List of intentions comes from user input
   // Store intentions in the parent (App) component
     // pass down to the child (userIntention) component
     // Initialize a state to keep track of the intentions as they are added or removed
   // Create a userIntention component which will display each individual intention
-  // each intention may have an X button which removes it from the list
+  // each intention will have a delete button which removes it from the list
 
-  // FRAMEWORK
+  // PARTS --
   // *** App Component ***
   // Two local methods (handleChange) to handle the onChange events to update states with user input:
   // - userIntention
@@ -30,13 +33,16 @@ function App() {
 
   // Render the application
   // - header component
-  // - two forms with user input
+  // - a form for user input
+  // - a date picker that is connected to input upon submit (to log in the db too)
   // - one HTML/CSS section on the page which is activated upon click / hover
   // - use imported Result and Date components
   // - footer component
 
   // setting states
   const [ intentionArray, setIntentionArray ] = useState([]);
+
+  const [ handleDate, setHandleDate ] = useState(null);
 
   const [ textInput, setTextInput ] = useState('');
 
@@ -56,7 +62,8 @@ function App() {
       for (let intentionKey in intentionData) {
         intentionsInState.push({
           uniqueKey: intentionKey,
-          intention: intentionData[intentionKey]
+          todaysDate: intentionData[intentionKey].date,
+          intention: intentionData[intentionKey].text,
         });
       }
       setIntentionArray(intentionsInState);
@@ -69,12 +76,27 @@ function App() {
     setTextInput(event.target.value);
   }
 
-  // validate input
+  // const handleDate = (event) => {
+  //   setHandleDate(event.target.value);
+  // }
+
+  // validate input and push data into database
   const handleSubmit = (event) => {
     event.preventDefault();
     const isValid = formValidation();
+
+    // In order to put the date into the db too, we parsed the date from the handleDate variable:
+    const date = handleDate.toDateString();
+
+    // we put the data into an object, and passed in the date and text properties:
+    const dataObject = {
+    date: date,
+    text: textInput,
+    }
+
+    // we push in the dataObject to the db:
     if(isValid){
-      dbRef.push(textInput);
+      dbRef.push(dataObject);
       setTextInput("");
     }
   }
@@ -103,6 +125,7 @@ function App() {
       <Header />
 
       <main>
+        <DatePicker selected={handleDate} onChange={date => setHandleDate(date)} dateFormat='MM/dd/yyyy' minDate={new Date()} placeholderText={`Choose date`} required/>
         <form action="" onSubmit={handleSubmit}>
           <label htmlFor="intentionInput">
             What one word, or feeling, would you like to move through this day
@@ -118,17 +141,20 @@ function App() {
           {Object.keys(textInputErr).map((key) => {
             return <div style={{ color: "red" }}>{textInputErr[key]}</div>;
           })}
-          <button>Embark</button>
+          <button>Log Entry</button>
         </form>
 
         <ul className="intentionLog">
           {intentionArray.map((item) => {
             return (
               <li key={item.uniqueKey}>
+                {/* we pass in the date value to also show on the page */}
+                <span>{item.todaysDate} </span>
                 <span>{item.intention}</span>
                 <button
                   onClick={() => {
                     handleClick(item.uniqueKey);
+                    // handleDate(date);
                   }}
                 >
                   Delete
